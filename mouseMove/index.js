14e90bf9ab9e6c639013279d932ee7e4cefd5b32
tinkerbell93 <hhjj105@gmail.com>
2021-01-24 21:42:51 +0900

    update mouse move interaction

--- mouseMove/index.js
+++ mouseMove/index.js
@@ -3,20 +3,25 @@ let y = 0;
 let moveX = 0;
 let moveY = 0;
 let speed = 0.05;
-let mouse;
-let h1 = document.getElementsByTagName('h1')[0];
+let $mouse = document.querySelector('.mouse');
+let $h1 = document.getElementsByTagName('h1')[0];
 
-mouse = document.getElementsByClassName('mouse')[0];
-
-window.addEventListener('mousemove', mouseFunc, false);
-
-// window.addEventListener('click', mouseFunc, false);
+function customMouse(x, y, scale = 1) {
+  $mouse.style.transform = `translate(${x}px, ${y}px) `;
+}
 
 function mouseFunc(e) {
   x = e.clientX;
   y = e.clientY;
-  mouse.style.transform = `translate(${x}px, ${y}px)`;
+  $mouse.style.transform = `translate(${x}px, ${y}px)`;
 }
+
+function mouseHoverFunc({ target }) {
+  target.matches('a') ? $mouse.classList.add('active') : $mouse.classList.remove('active');
+}
+
+window.addEventListener('mousemove', mouseFunc);
+window.addEventListener('mouseover', mouseHoverFunc);
 
 // loop();
 function loop() {
@@ -29,7 +34,7 @@ function loop() {
   // moveX = (3 - 2.81) * 0.5 + 2.81
   // moveX = (3 - 2.9) * 0.5 + 2.9
   moveY += (y - moveY) * speed;
-  // h1.innerHTML = `x: ${x}, moveX: ${moveX}`;
+  // $h1.innerHTML = `x: ${x}, moveX: ${moveX}`;
   mouse.style.transform = `translate(${moveX}px, ${moveY}px)`;
 
   window.requestAnimationFrame(loop);
